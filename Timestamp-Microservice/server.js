5ce6bc648f2513e0281e5a600be654a9164d2e24
aa-deet-eeya <aditya99mondal@gmail.com>
2020-06-27 22:31:50 +0530

    added final solution

--- Timestamp-Microservice/server.js
+++ Timestamp-Microservice/server.js
@@ -13,24 +13,41 @@ app.get('/' , (req ,res)=>{
 app.use(express.static( __dirname +'/public/')) ;
 
 
-app.get('/api/timestamp/:date_string' , (req ,res)=>{
-    //console.log(new Date(parseInt(req.params.date_string))) ;
-    date = new Date(parseInt(req.params.date_string)) ; 
-        
+app.get('/api/timestamp/', (req , res)=> {
+    const d = new Date() ;
+    res.json({
+      "unix" : parseInt(d.getTime()) ,
+      "utc" : d.toUTCString() 
+    }) ;
+  }) ;
+  
+  
+  app.get('/api/timestamp/:date_string' , (req ,res)=>{
+      //console.log(new Date(parseInt(req.params.date_string))) ;
     
-    res.json(isDate(date)) ;
-})  ;
-
-let isDate = (date)=> {
-    if(!isNaN(date.getTime())) {
-        return  {
-            "unix" : date.getTime() ,
-            "utc" : date.toUTCString() 
-        } ;
-    } 
-    return { "error" : "Invalid Date" } ;
-
-}
+      const date = new Date((req.params.date_string)) ; 
+      const dateString = req.params.date_string ;
+      if (/\d{5,}/.test(dateString)) {
+      const dateInt = parseInt(dateString);
+      
+      res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+    }    
+      
+      res.json(isDate(date)) ;
+  })  ;
+  
+  let isDate = (date)=> {
+      
+      if(!isNaN(date.getTime())) {
+          return  {
+              "unix" : (date.getTime().valueOf()) ,
+              "utc" : date.toUTCString() 
+          } ;
+      } 
+      return { "error" : "Invalid Date" } ;
+  
+  }
+  
 
 
 
